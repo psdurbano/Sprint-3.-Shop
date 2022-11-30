8267e9cb4d40461968a28041dd3f233d93a68788
Pablo Durbano <pablosdurbano@gmail.com>
2022-11-30 13:44:06 +0100

    exercice 4 halfway

--- js/shop.js
+++ js/shop.js
@@ -8,69 +8,87 @@ var total = 0;
 
 // Exercise 1
 function buy(id) {
-
-    for (let i = 0; i < products.length; i++) {
-        if (products[i].id == id) {
-          cartList.push(products[i]);
-          console.log(cartList);
-        }
-      }
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id == id) {
+      cartList.push(products[i]);
+      generateCart();
+    }
+  }
+  // 1. Loop for to the array products to get the item to add to cart
+  // 2. Add found product to the cartList array
 }
 
 // Exercise 2
 function cleanCart() {
-    cart.length = 0;
-    cartList.length = 0;
-    console.log(cartList);
+  cart.length = 0;
+  cartList.length = 0;
+  console.log(cartList);
 }
 
 // Exercise 3
 function calculateTotal() {
-    total = 0;
+  total = 0;
 
-    for (i = 0; i < cartList.length; i++){
-        total += cartList[i].price;
-    }
-    console.log(total);
-    return total;
-    // Calculate total price of the cart using the "cartList" array
+  for (i = 0; i < cartList.length; i++) {
+    total += cartList[i].price;
+  }
+  console.log(total);
+  return total;
+  // Calculate total price of the cart using the "cartList" array
 }
 
 // Exercise 4
-function generateCart() {
-    // Using the "cartlist" array that contains all the items in the shopping cart, 
-    // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
-}
+ /*   function generateCart() {
+  const cart = cartList.map((cartList) => {
+    return {
+      ...cartList,
+      quantity: 0,
+      subtotal: 0,
+      subtotalWithDiscount: 0,
+    };
+  });
+
+  for (let i = 0; i < cartList.length; i++) {
+    let IndiceItem = cart.findIndex(item => item.id === cartList[i].id);
+    if (IndiceItem === -1) {
+      cart.push(cart[i]);
+    } else {
+      cart[IndiceItem].quantity++;
+    }
+  }  console.log(cart);
+  
+
+
+  // Using the "cartlist" array that contains all the items in the shopping cart,
+  // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+}*/
 
 // Exercise 5
 function applyPromotionsCart() {
-    // Apply promotions to each item in the array "cart"
+  // Apply promotions to each item in the array "cart"
 }
 
 // Exercise 6
 function printCart() {
-    // Fill the shopping cart modal manipulating the shopping cart dom
+  // Fill the shopping cart modal manipulating the shopping cart dom
 }
-
 
 // ** Nivell II **
 
 // Exercise 7
 function addToCart(id) {
-    // Refactor previous code in order to simplify it 
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cart array or update its quantity in case it has been added previously.
+  // Refactor previous code in order to simplify it
+  // 1. Loop for to the array products to get the item to add to cart
+  // 2. Add found product to the cart array or update its quantity in case it has been added previously.
 }
 
 // Exercise 8
 function removeFromCart(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+  // 1. Loop for to the array products to get the item to add to cart
+  // 2. Add found product to the cartList array
 }
 
-function open_modal(){
-	console.log("Open Modal");
-	printCart();
+function open_modal() {
+  console.log("Open Modal");
+  printCart();
 }
